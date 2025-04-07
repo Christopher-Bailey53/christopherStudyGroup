6e5e53db5faf7687c0dac6e3c59e038b97e1848f
Christopher-Bailey53 <u24661092@tuks.co.za>
2025-04-07 09:31:06 +0200

    Prac4

---
+++ christopherStudyGroup/Scripts/MyScript.js
@@ -0,0 +1,25 @@
+﻿function deleteRow(button) {
+
+    var row = button.closest('tr');
+
+    row.remove();
+}
+
+function changeTextColor() {
+
+    var rows = document.querySelectorAll('#table tr');
+
+   
+    for (var i = 1; i < rows.length; i++) {
+        var row = rows[i];
+
+      
+        var nameCell = row.cells[1]; 
+
+       
+        if (nameCell && nameCell.innerText && nameCell.innerText.charAt(0).toUpperCase() === 'B') {
+          
+            row.style.color = 'red';
+        }
+    }
+}
